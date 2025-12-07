6feec283d5c08a5e993c5fd19a857fb10ca712a5
Cvives1 <cvivolm@etsinf.upv.es>
2025-12-07 14:44:54 +0100

    Añadida implementación de la clase BSTreeDict

---
+++ BSTreeDict.h
@@ -0,0 +1,40 @@
+#ifndef BSTREEDICT_H
+#define BSTREEDICT_H
+
+#include <ostream>
+#include <stdexcept>
+#include "Dict.h"
+#include "BSTree.h"
+#include "TableEntry.h"
+
+template <typename V>
+class BSTreeDict: public Dict<V> {
+
+    private:
+        BSTree<TableEntry<V>>* tree ;
+
+    public:
+        BSTreeDict(){
+	tree = new BSTree<TableEntry<V>>();
+	}
+	~BSTreeDict(){
+	delete tree;
+	}
+	friend std::ostream& operator<<(std::ostream &out, const BSTreeDict<V> &bs){
+	 bs.tree->inorder([&out](const TableEntry<V> &te){out << "('" << te.key << "' => " << te.value << ") "; });
+	 return out;
+	}
+	
+
+	V operator[](std::string key){
+	return search(key);
+	}
+
+	int entries() const override;          // llama tree->size()
+    	void insert(std::string key, V value) override;  // llama tree->insert(TableEntry)
+    	V search(std::string key) const override;       // llama tree->search(TableEntry)
+   	V remove(std::string key) override;             // llama tree->remove(TableEntry)
+        
+};
+
+#endif
